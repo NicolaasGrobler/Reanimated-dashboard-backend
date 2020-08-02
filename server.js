9a277a1635c2fb739af5a9d6cd1446aac26a0792
unknown <nicolaasgrob@gmail.com>
2020-08-02 19:21:58 +0200

    Events
    Post event for createing an Event;
    Get event for getting a single Event;

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
 const sql = require('mysql');
 const app = express();
 
@@ -16,6 +18,10 @@ connection.connect((err) => {
 
 console.log(`Server running on port: ${process.env.PORT}`);
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res, next) => {
     res.send('Request working');
 });
@@ -24,6 +30,26 @@ app.get('/getEvents', (req, res, next) => {
     connection.query('SELECT * FROM covid_screening.events', (err, result, fields) => {
         res.send(result);
     });    
+});
+
+app.get('/getEvent/:id', (req, res, next) => {
+    connection.query('SELECT * FROM covid_screening.events WHERE id = ? LIMIT 1', req.params.id, (err, result, fields) => {
+        res.send(result);
+    }); 
+});
+
+app.post('/createEvent', (req, res, next) => {
+    let eventName, eventDate, eventPlace, eventURL;
+
+    eventName = req.body.eventName;
+    eventDate = req.body.eventDate;
+    eventPlace = req.body.eventPlace;
+    eventURL = req.body.eventURL;
+
+    connection.query(`INSERT INTO covid_screening.events (event_name, event_place, event_date, event_img) VALUES (?, ?, ?, ?)`, [eventName, eventPlace, eventDate, eventURL], (err, result, fields) => {
+        if (err) throw err;
+        res.send(`Success! New row number: ${result.insertId}`);
+    }); 
 });
 
 app.listen(process.env.PORT);
